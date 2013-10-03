94e8be4ef37c626af0c8633a6eee69040a580d88
Tom Tromey <tromey@redhat.com>
2013-10-03 12:32:18 -0600

    add pwdhack.py and update Total-merged-user-map

---
+++ pwdhack.py
@@ -0,0 +1,19 @@
+# read sware-passwd file and missed-users and
+# compute a user map.
+
+import sys
+
+passwdmap = {}
+with open('sware-passwd') as f:
+    for line in f:
+        line = line.rstrip()
+        vec = line.split(':')
+        passwdmap[vec[0]] = vec[4]
+
+with open('missed-users') as f:
+    for line in f:
+        line = line.rstrip()
+        if line not in passwdmap:
+            print line
+            sys.exit(1)
+        print '%s <%s@sourceware.org>' % (passwdmap[line], line)
